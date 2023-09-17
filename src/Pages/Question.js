9ca7db542bf6295b1734d28328fba16aac187dfa
Vigneshsaravanan008 <vigneshsaravanan008@gmail.com>
2023-09-17 12:52:23 +0530

    question added

--- src/Pages/Question.js
+++ src/Pages/Question.js
@@ -1,60 +1,64 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import './Question.css';
 
 export default function Question() {
     const [question, setQuestion] = useState([]);
-    const getQuestion = (responseData) => {
-        var temp = [];
-        responseData.map((value, index) => {
-            console.log(value);
-            temp.push(value);
-        })
-        setQuestion([...question, ...temp]);
-    }
+    const [active, setActive] = useState(1);
+
     useEffect(() => {
         axios.get("https://the-trivia-api.com/v2/questions").then((response) => {
-            getQuestion(response.data);
+            setQuestion(response.data);
         })
     }, [])
     return (
-        <div className="container">
-            Questions
+        <div className='container'>
+            Questions Data
             <div className="row">
                 <div className="col-md-9">
                     <ul>
                         {question.map((value, key) => {
                             return (
-                                <div>
-                                    <li key={key}>
+                                <Card className={(active===key+1)?"m-4 p-2":"m-4 p-2 d-none"}>
+                                    <div key={key + 1}>
                                         {value?.question?.text}
-                                    </li>
+                                    </div>
                                     {
                                         value.incorrectAnswers.map((value2, key2) => {
                                             return (
-                                                <div>
+                                                <div key={key2}>
                                                     <input type="checkbox"></input>
-                                                    <label>{value2}</label>
+                                                    <label className='px-3'>{value2}</label>
                                                 </div>
                                             )
                                         })
                                     }
-                                     <div>
+                                    <div>
                                         <input type="checkbox"></input>
-                                        <label>{value.correctAnswer}</label>
+                                        <label className='px-3'>{value.correctAnswer}</label>
                                     </div>
-                                </div>
+                                </Card>
                             )
                         })}
                     </ul>
                 </div>
                 <div className="col-md-3">
-                    {question.map((value, key) => {
-                        return (
-                            <span key={key}>
-                                {key}
-                            </span>
-                        )
-                    })}
+                    <div className='border-no text-center py-2'>
+                        Question No
+                        <Row>
+                            {question.map((value, key) => {
+                                return (
+                                    <Col sm={4} className='py-3 px-3' key={key}>
+                                        <div className={(active === key + 1) ? "question_colot getquestion text-center py-4" : "getquestion text-center py-4"} onClick={() => { setActive(key + 1) }}>
+                                            {key + 1}
+                                        </div>
+                                    </Col>
+                                )
+                            })}
+                        </Row>
+                    </div>
                 </div>
             </div>
         </div>
